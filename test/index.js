05decc440c043c9a743f72a962fd7f19e55f5990
Andrew Maurer <git@maurer.me>
2022-12-18 13:11:53 -0500

    fixes to controls

--- test/index.js
+++ test/index.js
@@ -3,12 +3,23 @@ const motor = require('../lib/motor.js');
 
 describe('Motor', function () {
 
+
+
   describe('increase speeds', function () {
     
-    it('should begin spinning the wheels', function () {
-      const listener = motor.on('speedChanged', assert)
-      motor.emit('setSpeed', { direction: motor.FORWARD, speed: 20 })
-      motor.removeListener(listener)
+    it('should begin spinning the wheels', (done) => {
+      const direction = motor.FORWARD
+      const speed = 20
+      motor.emit('setSpeed', { direction, speed })
+      motor.on('speedChanged', (speedChanged) => {
+        assert(speed <= speedChanged.speed, "speed should be less or equal to requested speed")
+      })
+      motor.on('speedChangeComplete', () => {
+        assert(direction === speedChanged.direction, "direction should be equal to set")
+        assert(speed === speedChanged.speed, "speed should be equal to set")
+        motor.removeListener('speedChanged', this)
+        done()
+      })
     });
 
   });
