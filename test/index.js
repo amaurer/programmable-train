ff6fc05a1f896b979f19a4ef8208e725189ffb4e
Andrew Maurer <git@maurer.me>
2022-12-26 16:15:02 -0500

    change tests

--- test/index.js
+++ test/index.js
@@ -1,107 +1,113 @@
 const assert = require('assert')
-
-describe('Motor', function () {
-  this.timeout(20000)
-  this.motor = require('../lib/motor.js')
-  this.testSpeed = this.motor.MAX_SPEED / 3
+const MOTOR = require('../lib/motor.js')
 
 
-  describe('forward', function () {
-    const direction = 'forward'
+let testSpeed = MOTOR.MAX_SPEED / 2
 
-    it('should begin spinning the wheels', (done) => {
-      const speed = this.testSpeed
-      this.motor.once('speedChangeComplete', (speedChanged) => {
-        assert(direction === speedChanged.direction, 'direction should be equal to set')
-        assert(speed === speedChanged.speed, 'speed should be equal to set')
-        done()
-      })
-      this.motor.emit('setSpeed', { direction, speed })
+
+;(async () => {
+
+
+
+  // TRY FORWARD
+  let direction = MOTOR.FORWARD
+
+  await t(done => {
+    const speed = testSpeed
+    MOTOR.once('speedChangeComplete', (speedChanged) => {
+      assert(direction === speedChanged.direction, 'direction should be equal to set')
+      assert(speed === speedChanged.speed, 'speed should be equal to set')
+      done()
     })
+    MOTOR.emit('setSpeed', { direction, speed })
+  })
 
-    it('should slow the wheels to a stop', (done) => {
-      const speed = this.motor.MIN_SPEED
-      this.motor.once('speedChangeComplete', (speedChanged) => {
-        assert(direction === speedChanged.direction, 'direction should be equal to set')
-        assert(speed === speedChanged.speed, 'speed should be equal to set')
-        done()
-      })
-      this.motor.emit('setSpeed', { direction, speed })
+  await t(done => {
+    const speed = MOTOR.MIN_SPEED
+    MOTOR.once('speedChangeComplete', (speedChanged) => {
+      assert(direction === speedChanged.direction, 'direction should be equal to set')
+      assert(speed === speedChanged.speed, 'speed should be equal to set')
+      done()
     })
+    MOTOR.emit('setSpeed', { direction, speed })
+  })
 
-    it('should reverse max speed', (done) => {
-      const speed = this.motor.MAX_SPEED
-      this.motor.once('speedChangeComplete', (speedChanged) => {
-        assert(direction === speedChanged.direction, 'direction should be equal to set')
-        assert(speed === speedChanged.speed, 'speed should be equal to set')
-        done()
-      })
-      this.motor.emit('setSpeed', { direction, speed })
+  await t(done => {
+    const speed = MOTOR.MAX_SPEED
+    MOTOR.once('speedChangeComplete', (speedChanged) => {
+      assert(direction === speedChanged.direction, 'direction should be equal to set')
+      assert(speed === speedChanged.speed, 'speed should be equal to set')
+      done()
     })
+    MOTOR.emit('setSpeed', { direction, speed })
+  })
 
-    it('should stop', (done) => {
-      this.motor.once('speedChangeComplete', (speedChanged) => {
-        assert(direction === speedChanged.direction, 'direction should be equal to set')
-        assert(this.motor.MIN_SPEED === speedChanged.speed, 'speed should be equal to set')
-        done()
-      })
-      // Wait 1 sec
-      setTimeout(() => {
-        this.motor.emit('stop')
-      }, 1000)
+  await t(done => {
+    MOTOR.once('speedChangeComplete', (speedChanged) => {
+      assert(direction === speedChanged.direction, 'direction should be equal to set')
+      assert(MOTOR.MIN_SPEED === speedChanged.speed, 'speed should be equal to set')
+      done()
     })
-
-
+    // Wait 1 sec
+    setTimeout(() => {
+      MOTOR.emit('stop')
+    }, 1000)
   })
 
 
-  describe('reverse', function () {
-    const direction = 'reverse'
 
-    it('should begin spinning the wheels', (done) => {
-      const speed = this.testSpeed
-      this.motor.once('speedChangeComplete', (speedChanged) => {
-        assert(direction === speedChanged.direction, 'direction should be equal to set')
-        assert(speed === speedChanged.speed, 'speed should be equal to set')
-        done()
-      })
-      this.motor.emit('setSpeed', { direction, speed })
-    })
+  // TRY REVERSE
+  direction = MOTOR.BACKWARD
 
-    it('should slow the wheels to a stop', (done) => {
-      const speed = this.motor.MIN_SPEED
-      this.motor.once('speedChangeComplete', (speedChanged) => {
-        assert(direction === speedChanged.direction, 'direction should be equal to set')
-        assert(speed === speedChanged.speed, 'speed should be equal to set')
-        done()
-      })
-      this.motor.emit('setSpeed', { direction, speed })
+  await t(done => {
+    const speed = testSpeed
+    MOTOR.once('speedChangeComplete', (speedChanged) => {
+      assert(direction === speedChanged.direction, 'direction should be equal to set')
+      assert(speed === speedChanged.speed, 'speed should be equal to set')
+      done()
     })
+    MOTOR.emit('setSpeed', { direction, speed })
+  })
 
-    it('should reverse max speed', (done) => {
-      const speed = this.motor.MAX_SPEED
-      this.motor.once('speedChangeComplete', (speedChanged) => {
-        assert(direction === speedChanged.direction, 'direction should be equal to set')
-        assert(speed === speedChanged.speed, 'speed should be equal to set')
-        done()
-      })
-      this.motor.emit('setSpeed', { direction, speed })
+  await t(done => {
+    const speed = MOTOR.MIN_SPEED
+    MOTOR.once('speedChangeComplete', (speedChanged) => {
+      assert(direction === speedChanged.direction, 'direction should be equal to set')
+      assert(speed === speedChanged.speed, 'speed should be equal to set')
+      done()
     })
+    MOTOR.emit('setSpeed', { direction, speed })
+  })
 
-    it('should stop', (done) => {
-      this.motor.once('speedChangeComplete', (speedChanged) => {
-        assert(direction === speedChanged.direction, 'direction should be equal to set')
-        assert(this.motor.MIN_SPEED === speedChanged.speed, 'speed should be equal to set')
-        done()
-      })
-      // Wait 1 sec
-      setTimeout(() => {
-        this.motor.emit('stop')
-      }, 1000)
+  await t(done => {
+    const speed = MOTOR.MAX_SPEED
+    MOTOR.once('speedChangeComplete', (speedChanged) => {
+      assert(direction === speedChanged.direction, 'direction should be equal to set')
+      assert(speed === speedChanged.speed, 'speed should be equal to set')
+      done()
     })
+    MOTOR.emit('setSpeed', { direction, speed })
+  })
+
+  await t(done => {
+    MOTOR.once('speedChangeComplete', (speedChanged) => {
+      assert(direction === speedChanged.direction, 'direction should be equal to set')
+      assert(MOTOR.MIN_SPEED === speedChanged.speed, 'speed should be equal to set')
+      done()
+    })
+    // Wait 1 sec
+    setTimeout(() => {
+      MOTOR.emit('stop')
+    }, 1000)
+  })
     
 
-  })
+
+})()
 
 
-})
+
+
+function t(cb){
+  return new Promise(cb)
+}
